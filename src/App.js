4cb2480a90e1440632d0d37f690275be027dec19
Finn <finnbutler1@gmail.com>
2021-01-09 00:19:48 +0000

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import background from './background.mp4';
+import Button from "react-bootstrap/Button";
+import {FaLinkedinIn, FaGithub, FaRegEnvelope} from "react-icons/fa";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="video-container">
+          <video autoPlay playsInline muted loop className='video'>
+              <source src ={background} type="video/mp4" />
+          </video>
+          <header>
+
+          <div className="overlay mx-auto">
+              <Col className="column">
+                      <h1>
+                          Finn Butler
+                      </h1>
+                          <Button onClick={() => window.open( "https://github.com/FinnB1", "_blank")} className="mx-2 custom-button">
+                              <FaGithub/>
+                          </Button>
+                         <Button onClick={() => window.open( "https://www.linkedin.com/in/finnbutler1/", "_blank")} className="mx-2 custom-button">
+                              <FaLinkedinIn/>
+                         </Button>
+                        <Button onClick={() => window.open( "mailto:finn.butler@warwick.ac.uk", "_blank")} className="mx-2 custom-button">
+                            <FaRegEnvelope/>
+                        </Button>
+              </Col>
+
+          </div>
+
+          </header>
+      </div>
   );
 }
 
